dd810dcc1b1a6481aec2557ff2e69bbe9d621c09
Shubham <ssp671995@gmail.com>
2022-05-13 00:56:46 +0530

    Ability for greeting app to save message to save greeting message in repositry

--- src/main/java/com/bridgelabz/greetingapplication/model/User.java
+++ src/main/java/com/bridgelabz/greetingapplication/model/User.java
@@ -1,26 +1,48 @@
 package com.bridgelabz.greetingapplication.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String firstName;
     private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public User() {
 
     }
+
     public User(User user) {
-        this.firstName=user.firstName;
-        this.lastName=user.lastName;
+        this.id = user.id;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
     }
+
+
     public String getfirstName() {
         return firstName;
     }
-    public void setfName(String fName) {
-        this.firstName = fName;
+
+    public void setfName(String firstName) {
+        this.firstName = firstName;
     }
+
     public String getlastName() {
         return lastName;
     }
-    public void setlastName(String lName) {
+
+    public void setlastName(String lastName) {
         this.lastName = lastName;
     }
 
--- src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.greetingapplication.service;
 
+import com.bridgelabz.greetingapplication.model.Greeting;
+import com.bridgelabz.greetingapplication.model.User;
+import com.bridgelabz.greetingapplication.repository.GreetingAppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+    private static String template = "Hello %s";
+    @Autowired
+    GreetingAppRepository repository;
 
     public String getMessage() {
         return "Hello World";
@@ -13,7 +20,13 @@ public class GreetingService {
         return "Hello " + firstName + "" + lastName;
     }
 
-    public String postMessage(String firstName, String lastName) {
-        return "Hello " + firstName + "" + lastName;
+    public String postMessage(User user) {
+        return "Hello " + user.getfirstName() + "" + user.getlastName();
+    }
+
+    public Greeting saveMessage(Greeting greeting) {
+        Greeting newGreeting = new Greeting(String.format(template, greeting.getContent()));
+        repository.save(newGreeting);
+        return newGreeting;
     }
 }
